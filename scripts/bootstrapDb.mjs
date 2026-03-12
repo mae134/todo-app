63ac099642d537ad748c30d8e95987a159fe98a8
mae134 <en.tps.luc@gmail.com>
2026-03-12 17:38:02 +0900

    fix: app processing

--- scripts/bootstrapDb.mjs
+++ scripts/bootstrapDb.mjs
@@ -1,5 +1,5 @@
-import fs from 'fs'
-import path from 'path'
+import fs from 'node:fs/promises'
+import path from 'node:path'
 
 // DBフォルダのパスを決める 絶対パスを取得
 const dbDir = path.resolve('../todo-local-db')
