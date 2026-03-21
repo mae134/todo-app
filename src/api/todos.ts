fc95b277de61f343f00114830cda530827f7f122
mae134 <en.tps.luc@gmail.com>
2026-03-21 21:50:04 +0900

    refactor: Delete unnecessary comments

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,9 +1,5 @@
-// import * as localApi from './todos.local'
 import * as supabaseApi from './todos.supabase'
 
-// const todoApi = import.meta.env.DEV ? localApi : supabaseApi
-
-// 一時的に Supabase を使う
 const todoApi = supabaseApi
 
 export const {
